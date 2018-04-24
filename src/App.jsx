27cc2c934937ace1b924cf56536ec42e8ca61136
Yue Wang <1087691+yuewang@users.noreply.github.com>
2018-04-24 18:28:46 -0400

    hopefully i committed the right ones

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+var styles = {
+    color:'red'
+}
+
 var contentNode = document.getElementById('contents');
-var component = <h1>Hello World</h1>;
+var component = <h1 style={styles}>Hello World</h1>;
 ReactDOM.render(component, contentNode);
