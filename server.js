ed6b03a80d13916c10062eabc1fc1dd9c2dbb006
Yue Wang <1087691+yuewang@users.noreply.github.com>
2018-04-24 23:16:36 -0400

    before polyfill (2-8) exercise

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 
 const app = express();
+
 app.use(express.static('static'));
-//app.use(express.static('public'));
+//app.use(express.static('public')); //changed folder name back to static for tutorial
 
 app.listen(3000, function() {
     console.log('App started on port 3000');
--- static/App.js
+++ static/App.js
@@ -1,7 +1,11 @@
+var styles = {
+    color: 'red'
+};
+
 var contentNode = document.getElementById('contents');
 var component = React.createElement(
-  'h1',
-  null,
-  'Hello World'
+    'h1',
+    { style: styles },
+    'Hello World'
 );
 ReactDOM.render(component, contentNode);
